b5294eb73396eab7420f90c2283a0d258da4d68b
Rui Moreira <ruizinho@Mac.home.local>
2025-04-30 16:57:37 +0200

    Had to restructure the whole code because I was missing the knowledge on how to set a variable in .js to use in .css. Finally done - 16 or 100 squares occupy the same space

--- etch.js
+++ etch.js
@@ -1,16 +1,29 @@
-const container = document.querySelector(".container");
-
-
-
-const box = document.querySelector('.container');
-for(let i =1;i<273;i++){
-    const div = document.createElement('div');
-    box.appendChild(div);
-}
-
+const container = document.querySelector('.container');
 const btn = document.querySelector('#btn');
+let sides = 16;
 
-btn.addEventListener("click",()=> {
-    let size = prompt("What size would you like?")
-    return size;
-})
+btn.addEventListener("click", () => {
+  let size = prompt("What size should the grid be?");
+  sides = parseInt(size);
+
+  if (sides > 100) {
+    alert("Too big! Try smaller!")
+  } else {
+    createGrid();
+  }
+});
+
+
+
+function createGrid() {
+  container.replaceChildren();
+  
+  for (let i = 1; i <= (sides * sides); i++) {
+    const box = document.createElement('div');       
+    container.appendChild(box);
+  }
+  
+  document.documentElement.style.setProperty('--number', sides);
+};
+
+createGrid();
