fd57a07d524170fb2b2ff8333acdee2d914067c8
Rui Moreira <ruizinho@Mac.home.local>
2025-04-30 09:26:14 +0200

    created a for loop to create the necessary number of boxes for the 16x16 grid

---
+++ etch.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+
+
+const box = document.querySelector('.container');
+for(let i =1;i<273;i++){
+    const div = document.createElement('div');
+    box.appendChild(div);
+}
+
+
+
